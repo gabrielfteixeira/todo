4b50e06bcd472c370fe19ec62a439507eb086c3b
Gabriel Teixeira <gabriel.teixeira009@gmail.com>
2026-07-07 13:47:22 -0300

    feature: navegabilidade dentro do sistema

--- src/App.jsx
+++ src/App.jsx
@@ -57,7 +57,7 @@ function App() {
   }
 
   return (
-    <div className="w-screen h-screen bg-gray-500 flex justify-center p-6">
+    <div className="w-screen h-screen bg-slate-500 flex justify-center p-6">
       <div className="w-[500px] space-y-4">
         <h1 className="text-3xl font-bold text-slate-100 text-center">
           Gerenciador de Tarefas
